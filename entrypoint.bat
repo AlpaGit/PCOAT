a22bdf5f5595ff62bbbdeb95c984358b16aae0d6
Alpa <alpaoff@gmail.com>
2023-08-16 20:25:17 +0200

    add

--- entrypoint.bat
+++ entrypoint.bat
@@ -1,7 +1,8 @@
-CALL .\DofusRecompiler.bat DofusInvoker.swf DofusInvoker_1.swf com.ankamagames.dofus.modules.utils.pathfinding.astar.AStar hasValidDestinationSubarea Astar_hasValidDestinationSubarea.pcode
-CALL .\DofusRecompiler.bat DofusInvoker_1.swf DofusInvoker_2.swf Ankama_Cartography.ui.CartographyBase hasAutoPilot CartographyBase_hasAutoPilot.pcode
-CALL .\DofusRecompiler.bat DofusInvoker_2.swf DofusInvoker_3.swf com.ankamagames.dofus.logic.game.common.managers.CharacterDisplacementManager autoTravel CharacterDisplacementManager_autoTravel.pcode
-CALL .\DofusRecompiler.bat DofusInvoker_3.swf DofusInvoker_4.swf com.ankamagames.dofus.misc.interClient.InterClientKeyManager generateKey InterClientKeyManager_generateKey.pcode
-CALL .\DofusRecompiler.bat DofusInvoker_4.swf DofusInvoker_5.swf com.ankamagames.dofus.misc.interClient.InterClientKeyManager pingNext InterClientKeyManager_pingNext.pcode
-CALL .\DofusRecompiler.bat DofusInvoker_5.swf DofusInvoker_6.swf com.ankamagames.dofus.logic.game.roleplay.managers.MountAutoTripManager initNewTrip MountAutoTripManager_initNewTrip.pcode
-CALL .\DofusRecompiler.bat DofusInvoker_6.swf DofusInvoker_7.swf com.ankamagames.dofus.misc.utils.CursorConstant init Cursor.pcode
+CALL .\DofusRecompiler.bat DofusInvoker.swf DofusInvoker_edit.swf com.ankamagames.dofus.modules.utils.pathfinding.astar.AStar hasValidDestinationSubarea Astar_hasValidDestinationSubarea.pcode
+CALL .\DofusRecompiler.bat DofusInvoker_edit.swf DofusInvoker_edit.swf com.ankamagames.dofus.misc.interClient.InterClientKeyManager generateKey InterClientKeyManager_generateKey.pcode
+CALL .\DofusRecompiler.bat DofusInvoker_edit.swf DofusInvoker_edit.swf com.ankamagames.dofus.misc.interClient.InterClientKeyManager pingNext InterClientKeyManager_pingNext.pcode
+CALL .\DofusRecompiler.bat DofusInvoker_edit.swf DofusInvoker_edit.swf com.ankamagames.dofus.logic.game.roleplay.managers.MountAutoTripManager initNewTrip MountAutoTripManager_initNewTrip.pcode
+CALL .\DofusRecompiler.bat DofusInvoker_edit.swf DofusInvoker_edit.swf com.ankamagames.dofus.network.messages.game.context.roleplay serializeAs_ChangeMapMessage ChangeMapMessage_serializeAs.pcode
+CALL .\DofusRecompiler.bat DofusInvoker_edit.swf DofusInvoker_edit.swf Ankama_Cartography.ui.CartographyBase hasAutoPilot CartographyBase_hasAutoPilot.pcode
+CALL .\DofusRecompiler.bat DofusInvoker_edit.swf DofusInvoker_edit.swf com.ankamagames.dofus.logic.game.common.managers.CharacterDisplacementManager autoTravel CharacterDisplacementManager_autoTravel.pcode
+CALL .\DofusRecompiler.bat DofusInvoker_edit.swf DofusInvoker_new.swf com.ankamagames.dofus.misc.utils.CursorConstant init Cursor.pcode
