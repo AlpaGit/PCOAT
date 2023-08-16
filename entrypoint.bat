3fba9c36308f525d0b1ad85615019ac737ccc4ca
Alpa <alpaoff@gmail.com>
2023-08-16 20:28:47 +0200

    add changemapmessage

--- entrypoint.bat
+++ entrypoint.bat
@@ -2,7 +2,7 @@ CALL .\DofusRecompiler.bat DofusInvoker.swf DofusInvoker_edit.swf com.ankamagame
 CALL .\DofusRecompiler.bat DofusInvoker_edit.swf DofusInvoker_edit.swf com.ankamagames.dofus.misc.interClient.InterClientKeyManager generateKey InterClientKeyManager_generateKey.pcode
 CALL .\DofusRecompiler.bat DofusInvoker_edit.swf DofusInvoker_edit.swf com.ankamagames.dofus.misc.interClient.InterClientKeyManager pingNext InterClientKeyManager_pingNext.pcode
 CALL .\DofusRecompiler.bat DofusInvoker_edit.swf DofusInvoker_edit.swf com.ankamagames.dofus.logic.game.roleplay.managers.MountAutoTripManager initNewTrip MountAutoTripManager_initNewTrip.pcode
-CALL .\DofusRecompiler.bat DofusInvoker_edit.swf DofusInvoker_edit.swf com.ankamagames.dofus.network.messages.game.context.roleplay serializeAs_ChangeMapMessage ChangeMapMessage_serializeAs.pcode
+CALL .\DofusRecompiler.bat DofusInvoker_edit.swf DofusInvoker_edit.swf com.ankamagames.dofus.network.messages.game.context.roleplay.ChangeMapMessage serializeAs_ChangeMapMessage ChangeMapMessage_serializeAs.pcode
 CALL .\DofusRecompiler.bat DofusInvoker_edit.swf DofusInvoker_edit.swf Ankama_Cartography.ui.CartographyBase hasAutoPilot CartographyBase_hasAutoPilot.pcode
 CALL .\DofusRecompiler.bat DofusInvoker_edit.swf DofusInvoker_edit.swf com.ankamagames.dofus.logic.game.common.managers.CharacterDisplacementManager autoTravel CharacterDisplacementManager_autoTravel.pcode
 CALL .\DofusRecompiler.bat DofusInvoker_edit.swf DofusInvoker_new.swf com.ankamagames.dofus.misc.utils.CursorConstant init Cursor.pcode
