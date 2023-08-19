239425cd8445262c853e2a738d74365379c120fa
Alpa <alpaoff@gmail.com>
2023-08-19 11:32:05 +0200

    fix

--- entrypoint.bat
+++ entrypoint.bat
@@ -1,8 +1,8 @@
-CALL .\DofusRecompiler.bat DofusInvoker.swf DofusInvoker.swf com.ankamagames.dofus.modules.utils.pathfinding.astar.AStar hasValidDestinationSubarea Astar_hasValidDestinationSubarea.pcode
-CALL .\DofusRecompiler.bat DofusInvoker.swf DofusInvoker.swf com.ankamagames.dofus.misc.interClient.InterClientKeyManager generateKey InterClientKeyManager_generateKey.pcode
-CALL .\DofusRecompiler.bat DofusInvoker.swf DofusInvoker.swf com.ankamagames.dofus.misc.interClient.InterClientKeyManager pingNext InterClientKeyManager_pingNext.pcode
-CALL .\DofusRecompiler.bat DofusInvoker.swf DofusInvoker.swf com.ankamagames.dofus.logic.game.roleplay.managers.MountAutoTripManager initNewTrip MountAutoTripManager_initNewTrip.pcode
-CALL .\DofusRecompiler.bat DofusInvoker.swf DofusInvoker.swf com.ankamagames.dofus.network.messages.game.context.roleplay.ChangeMapMessage serializeAs_ChangeMapMessage ChangeMapMessage_serializeAs.pcode
-CALL .\DofusRecompiler.bat DofusInvoker.swf DofusInvoker.swf Ankama_Cartography.ui.CartographyBase hasAutoPilot CartographyBase_hasAutoPilot.pcode
-CALL .\DofusRecompiler.bat DofusInvoker.swf DofusInvoker.swf com.ankamagames.dofus.logic.game.common.managers.CharacterDisplacementManager autoTravel CharacterDisplacementManager_autoTravel.pcode
-CALL .\DofusRecompiler.bat DofusInvoker.swf DofusInvoker.swf com.ankamagames.dofus.misc.utils.CursorConstant init Cursor.pcode
+CALL .\DofusRecompiler.bat DofusInvoker.swf DofusInvoker.swf com.ankamagames.dofus.modules.utils.pathfinding.astar.AStar hasValidDestinationSubarea Astar_hasValidDestinationSubarea.pcode false
+CALL .\DofusRecompiler.bat DofusInvoker.swf DofusInvoker.swf com.ankamagames.dofus.misc.interClient.InterClientKeyManager generateKey InterClientKeyManager_generateKey.pcode false
+CALL .\DofusRecompiler.bat DofusInvoker.swf DofusInvoker.swf com.ankamagames.dofus.misc.interClient.InterClientKeyManager pingNext InterClientKeyManager_pingNext.pcode false
+CALL .\DofusRecompiler.bat DofusInvoker.swf DofusInvoker.swf com.ankamagames.dofus.logic.game.roleplay.managers.MountAutoTripManager initNewTrip MountAutoTripManager_initNewTrip.pcode false
+CALL .\DofusRecompiler.bat DofusInvoker.swf DofusInvoker.swf com.ankamagames.dofus.network.messages.game.context.roleplay.ChangeMapMessage serializeAs_ChangeMapMessage ChangeMapMessage_serializeAs.pcode false
+CALL .\DofusRecompiler.bat DofusInvoker.swf DofusInvoker.swf Ankama_Cartography.ui.CartographyBase hasAutoPilot CartographyBase_hasAutoPilot.pcode false
+CALL .\DofusRecompiler.bat DofusInvoker.swf DofusInvoker.swf com.ankamagames.dofus.logic.game.common.managers.CharacterDisplacementManager autoTravel CharacterDisplacementManager_autoTravel.pcode false
+CALL .\DofusRecompiler.bat DofusInvoker.swf DofusInvoker.swf com.ankamagames.dofus.misc.utils.CursorConstant init Cursor.pcode false
